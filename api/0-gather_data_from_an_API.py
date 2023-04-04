c40ed26f668b9827f4cdb03addbedc0f6e417c03
Ethan1709 <benyayerethan@gmail.com>
2023-04-04 21:23:48 +0200

    adding task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -35,8 +35,8 @@ if __name__ == "__main__":
     NUMBER_OF_DONE_TASKS = i
     TOTAL_NUMBER_OF_TASKS = i + j
     TASK_TITLE = ""
-    print(f'Employee {EMPLOYEE_NAME} is done with tasks\
-{NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+    print('Employee {} is done with tasks({}/{}):'.format
+          (EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS))
     for i in li:
         TASK_TITLE = i
         print('\t ' + TASK_TITLE)
