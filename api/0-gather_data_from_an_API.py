9c32a22b9f12cb9670b0f1637db59ef2e140d25c
Ethan1709 <benyayerethan@gmail.com>
2023-04-04 19:45:27 +0200

    adding task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -11,23 +11,24 @@ todos = response_todos.json()
 
 for item in user:
     if 'id' in item and str(item['id']) == str(sys.argv[1]):
-            n = item['id']
+        n = item['id']
 n -= 1
 
 i = 0
 j = 0
-l =[]
+li = []
 for item in todos:
-      if 'userId' in item and str(item['userId']) == str(sys.argv[1]):
-            if item['completed'] is True:
-                  i += 1
-                  l.append(item['title'])
-            else:
-                  j += 1
+    if 'userId' in item and str(item['userId']) == str(sys.argv[1]):
+        if item['completed'] is True:
+            i += 1
+            li.append(item['title'])
+        else:
+            j += 1
 
 EMPLOYEE_NAME = user[n]['name']
 NUMBER_OF_DONE_TASKS = i
 TOTAL_NUMBER_OF_TASKS = i + j
-print(f'Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
-for i in l:
-      print('\t ' + i)
+print(f'Employee {EMPLOYEE_NAME} is done with\
+       tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+for i in li:
+    print('\t ' + i)
