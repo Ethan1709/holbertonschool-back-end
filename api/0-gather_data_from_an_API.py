98d7735bd6f5d63b5607ea3fab14f7a012453f9c
Ethan1709 <benyayerethan@gmail.com>
2023-04-04 20:53:15 +0200

    adding task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -7,34 +7,35 @@ a given employee ID, returns information about his/her TODO list progress.
 import requests
 import sys
 
+if __name__ == "__main__":
+    response_user = requests.get('https://jsonplaceholder.typicode.com/users')
+    response_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    user = response_user.json()
+    todos = response_todos.json()
 
-response_user = requests.get('https://jsonplaceholder.typicode.com/users')
-response_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
-user = response_user.json()
-todos = response_todos.json()
+    for item in user:
+        if 'id' in item and str(item.get('id')) == str(sys.argv[1]):
+            n = item.get('id')
+    n -= 1
 
-for item in user:
-    if 'id' in item and str(item.get('id')) == str(sys.argv[1]):
-        n = item.get('id')
-n -= 1
+    i = 0
+    j = 0
+    li = []
+    for item in todos:
+        if 'userId' in item and str(item.get('userId')) == str(sys.argv[1]):
+            if item.get('completed') is True:
+                i += 1
+                li.append(item.get('title'))
+            else:
+                j += 1
 
-i = 0
-j = 0
-li = []
-for item in todos:
-    if 'userId' in item and str(item.get('userId')) == str(sys.argv[1]):
-        if item.get('completed') is True:
-            i += 1
-            li.append(item.get('title'))
-        else:
-            j += 1
+    EMPLOYEE_NAME = user[n]['name']
+    NUMBER_OF_DONE_TASKS = i
+    TOTAL_NUMBER_OF_TASKS = i + j
+    TASK_TITLE = ""
 
-EMPLOYEE_NAME = user[n]['name']
-NUMBER_OF_DONE_TASKS = i
-TOTAL_NUMBER_OF_TASKS = i + j
-TASK_TITLE = ""
-print(f'Employee {EMPLOYEE_NAME} is done with tasks'
-      f'({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
-for i in li:
-    TASK_TITLE = i
-    print('\t ' + TASK_TITLE)
+    print(f'Employee {EMPLOYEE_NAME} is done with tasks'
+    f'({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+    for i in li:
+        TASK_TITLE = i
+        print('\t ' + TASK_TITLE)
