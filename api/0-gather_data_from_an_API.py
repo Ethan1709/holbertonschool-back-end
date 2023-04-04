dad21965234b92a092f27025cfc38d1832dbd53a
Ethan1709 <benyayerethan@gmail.com>
2023-04-04 19:37:45 +0200

    adding task 0

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,3 +1,34 @@
 #!/usr/bin/python3
 """API"""
 
+import requests
+import sys
+
+response_user = requests.get('https://jsonplaceholder.typicode.com/users')
+response_todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+user = response_user.json()
+todos = response_todos.json()
+#print(todos)
+
+for item in user:
+    if 'id' in item and str(item['id']) == str(sys.argv[1]):
+            n = item['id']
+n -= 1
+
+i = 0
+j = 0
+l =[]
+for item in todos:
+      if 'userId' in item and str(item['userId']) == str(sys.argv[1]):
+            if item['completed'] is True:
+                  i += 1
+                  l.append(item['title'])
+            else:
+                  j += 1
+
+EMPLOYEE_NAME = user[n]['name']
+NUMBER_OF_DONE_TASKS = i
+TOTAL_NUMBER_OF_TASKS = i + j
+print(f'Employee {EMPLOYEE_NAME} is done with tasks({NUMBER_OF_DONE_TASKS}/{TOTAL_NUMBER_OF_TASKS}):')
+for i in l:
+      print('\t ' + i)
