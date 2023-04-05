cd0ac5d29eae5689735fb1550838403cfb7e8da0
Ethan1709 <benyayerethan@gmail.com>
2023-04-05 03:54:23 +0200

    fckkkkk

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-Writes a Python script that, using this REST API, for
+Write a Python script that, using this REST API, for
 a given employee ID, returns information about his/her TODO list progress.
 """
 
